ffc59656d89ae9f2473123e16a8533bf8e44add2
Shalom91 <56162445+Shalom91@users.noreply.github.com>
2020-02-19 16:40:21 +0200

    Update test_person.py

--- test_person.py
+++ test_person.py
@@ -3,4 +3,4 @@ from person import Person
 
 def test_hello():
     person1 = Person("Molash", 40, "male", "being rich, watching anime, and reading novels.")
-    assert Person.hello(person1) == "Hello, my name is Molash and I am 40 years old. My interests are being rich, watching anime, and reading novels."
+    assert person1.hello() == "Hello, my name is Molash and I am 40 years old. My watching TV, and reading books."
